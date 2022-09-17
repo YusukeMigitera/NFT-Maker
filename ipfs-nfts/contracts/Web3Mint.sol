f76f6869e5dfcdbfc74112f866840329a3ef463e
YusukeMigitera <ymigitera@gmail.com>
2022-09-18 02:24:14 +0900

    section 2

--- ipfs-nfts/contracts/Web3Mint.sol
+++ ipfs-nfts/contracts/Web3Mint.sol
@@ -1,38 +1,69 @@
 // Web3Mint.sol
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./libraries/Base64.sol";
 import "hardhat/console.sol";
 
-contract Web3Mint is ERC721URIStorage {
+contract Web3Mint is ERC721 {
     string public phrase;
-    // OpenZeppelin が tokenIds を簡単に追跡するために提供するライブラリを呼び出しています
+
+    struct NftAttributes {
+        string name;
+        string imageURL;
+    }
+    NftAttributes[] Web3Nfts;
+
     using Counters for Counters.Counter;
-    // _tokenIdsを初期化（_tokenIds = 0）
     Counters.Counter private _tokenIds;
 
     // NFT トークンの名前とそのシンボルを渡します。
-    constructor() ERC721("TanyaNFT", "TANYA") {
+    constructor() ERC721("NFT", "nft") {
         phrase = "This is my NFT contract.";
         console.log(phrase);
     }
 
-    // ユーザーが NFT を取得するために実行する関数です。
-    function makeAnEpicNFT() public {
-        // 現在のtokenIdを取得します。tokenIdは0から始まります。
+    // ユーザーが NFT を取得するために実行する関数。imageURIにはIPFSのCIDを入れる
+    function mintIpfsNFT(string memory name, string memory imageURI) public {
         uint256 newItemId = _tokenIds.current();
-        // msg.sender を使って NFT を送信者に Mint します。
+        // EOAアドレスとトークンIDを紐付ける
         _safeMint(msg.sender, newItemId);
-        // NFT データを設定します。
-        _setTokenURI(newItemId, "http://localhost:3000");
-        // NFTがいつ誰に作成されたかを確認します。
+        Web3Nfts.push(NftAttributes({name: name, imageURL: imageURI}));
         console.log(
             "An NFT w/ ID %s has been minted to %s",
             newItemId,
             msg.sender
         );
-        // 次の NFT が Mint されるときのカウンターをインクリメントする。
         _tokenIds.increment();
+    }
+
+    // トークンIDを入れるとbase64エンコードしたmetadataを返す
+    // OpenSeaなどが利用する
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        Web3Nfts[_tokenId].name,
+                        " -- NFT #: ",
+                        Strings.toString(_tokenId),
+                        '", "description": "An epic NFT", "image": "ipfs://',
+                        Web3Nfts[_tokenId].imageURL,
+                        '"}'
+                    )
+                )
+            )
+        );
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        return output;
     }
 }
--- ipfs-nfts/test/test.js
+++ ipfs-nfts/test/test.js
@@ -5,18 +5,16 @@ describe("NFT-Maker", function () {
   async function deployNftMakerFixture() {
     const phrase = "This is my NFT contract.";
 
-    const [owner] = await ethers.getSigners();
+    const [owner, other1] = await ethers.getSigners();
     const ContractFactory = await ethers.getContractFactory("Web3Mint");
     const Contract = await ContractFactory.deploy();
 
-    return { Contract, owner, phrase };
+    return { Contract, owner, other1, phrase };
   }
 
   describe("Deployment", function () {
     it("Should set the right phrase", async function () {
-      const { Contract, phrase } = await loadFixture(
-        deployNftMakerFixture
-      );
+      const { Contract, phrase } = await loadFixture(deployNftMakerFixture);
 
       const contractPhrase = await Contract.phrase();
       expect(contractPhrase).to.equal(phrase);
@@ -26,14 +24,19 @@ describe("NFT-Maker", function () {
 
   describe("Mint", function () {
     it("Should mint", async function () {
-      const { Contract } = await loadFixture(
+      const { Contract, owner, other1 } = await loadFixture(
         deployNftMakerFixture
       );
 
-      let txn = await Contract.makeAnEpicNFT();
-      await txn.wait();
-      txn = await Contract.makeAnEpicNFT();
-      await txn.wait();
+      let nftName = "poker";
+      let ipfsCID =
+        "bafkreievxssucnete4vpthh3klylkv2ctll2sk2ib24jvgozyg62zdtm2y";
+
+      await Contract.connect(owner).mintIpfsNFT(nftName, ipfsCID); //0
+      await Contract.connect(other1).mintIpfsNFT(nftName, ipfsCID); //1
+
+      console.log(await Contract.tokenURI(0));
+      console.log(await Contract.tokenURI(1));
     });
   });
 });
